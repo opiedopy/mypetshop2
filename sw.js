b1ce5aff4484236f5e8ee5103c15f6f2bc395519
Opie <76084564+opiedopy@users.noreply.github.com>
2026-03-29 07:36:34 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,44 @@
+const CACHE_VERSION = 'v1'; //  Bump this to 'v2' to trigger update
+const CACHE_NAME = `mypetshop-cache-${CACHE_VERSION}`;
+const ASSETS = [
+    '/mypetshop2/',
+    '/mypetshop2/index.html',
+    '/mypetshop2/manifest.json',
+    '/mypetshop2/images/dog.png',
+    '/mypetshop2/images/cat.png',
+    '/mypetshop2/images/curler-hamster-icon.png'
+];
 
+// Install Event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+// Activate Event - Cleanup old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            })
+        ))
+    );
+});
+
+// Cache-first Strategy 
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Handle Update Banner click 
+self.addEventListener('message', event => {
+    if (event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
